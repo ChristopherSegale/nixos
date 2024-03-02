9037d7de0bb3e93efe79962d24958047f97f4465
ChristopherSegale <ccs.segale@gmail.com>
2024-03-01 21:21:53 -0500

    Removing redundant package

--- defaults/default.nix
+++ defaults/default.nix
@@ -37,12 +37,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     emacs
     ed
     wget
     tmux
-    git
     file
   ];
 

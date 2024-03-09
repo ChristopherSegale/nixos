73f9e2394b83bbfa047cffac5e88f28c793828b2
ChristopherSegale <ccs.segale@gmail.com>
2024-03-08 22:04:06 -0500

    Installing dependencies for EAF

--- hosts/user/baremetal/home.nix
+++ hosts/user/baremetal/home.nix
@@ -17,6 +17,24 @@ let
     yt-dlp
   ];
   custom-packages = [ snake.packages.x86_64-linux.default ];
+  # EAF dependencies
+  python-dependencies = (pkgs.python3.withPackages (python-pkgs: [
+    python-pkgs.pandas
+    python-pkgs.requests
+    python-pkgs.sexpdata
+    python-pkgs.tld
+    python-pkgs.pyqt6
+    python-pkgs.pyqt6-sip
+    python-pkgs.pyqt6-webengine python-pkgs.epc python-pkgs.lxml # for eaf
+    python-pkgs.qrcode # eaf-file-browser
+    python-pkgs.pysocks # eaf-browser
+    python-pkgs.pymupdf # eaf-pdf-viewer
+    python-pkgs.pypinyin # eaf-file-manager
+    python-pkgs.psutil # eaf-system-monitor
+    python-pkgs.retry # eaf-markdown-previewer
+    python-pkgs.markdown
+  ]));
+  eaf-packages = with pkgs; [ nodejs wmctrl xdotool python-dependencies ];
 in
 {
   home = {
@@ -26,7 +44,8 @@ in
     stateVersion = "24.05";
 
     packages = standard-packages ++
-               custom-packages;
+               custom-packages ++
+	       eaf-packages;
   };
   programs = {
     home-manager.enable = true;
@@ -38,7 +57,7 @@ in
     };
     emacs = {
       enable = true;
-      package = pkgs.emacs;
+      package = pkgs.emacs-gtk;
     };
   };
 }

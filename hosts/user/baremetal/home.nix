258e7628b044129db44c7cce3699ebe6a947e74c
ChristopherSegale <ccs.segale@gmail.com>
2024-03-17 19:29:05 -0400

    Reorganizing packages into a single nix file

--- hosts/user/baremetal/home.nix
+++ hosts/user/baremetal/home.nix
@@ -1,56 +1,9 @@
 { config, pkgs, user, snake, ... }:
 
 let
-  web-browsers = with pkgs; [
-    firefox-wayland
-    chromium
-  ];
-  virtualization = with pkgs; [
-    qemu
-  ];
-  containers = with pkgs; [
-    podman
-    distrobox
-  ];
-  productivity = with pkgs; [
-    libreoffice
-    mupdf
-    groff
-  ];
-  development-tools = with pkgs; [
-    plan9port
-    gnumake
-  ];
-  multimedia = with pkgs; [
-    mpv
-    yt-dlp
-  ];
-  misc = with pkgs; [
-    neofetch
-  ];
-  console-games = with pkgs; [
-    crawl
-    nethack
-  ];
+  inherit (import ../programs pkgs) web-browsers virtualization containers productivity
+                                    development-tools multimedia misc
   custom-packages = [ snake.packages.x86_64-linux.default ];
-  # EAF dependencies
-  python-dependencies = (pkgs.python3.withPackages (python-pkgs: [
-    python-pkgs.pandas
-    python-pkgs.requests
-    python-pkgs.sexpdata
-    python-pkgs.tld
-    python-pkgs.pyqt6
-    python-pkgs.pyqt6-sip
-    python-pkgs.pyqt6-webengine python-pkgs.epc python-pkgs.lxml # for eaf
-    python-pkgs.qrcode # eaf-file-browser
-    python-pkgs.pysocks # eaf-browser
-    python-pkgs.pymupdf # eaf-pdf-viewer
-    python-pkgs.pypinyin # eaf-file-manager
-    python-pkgs.psutil # eaf-system-monitor
-    python-pkgs.retry # eaf-markdown-previewer
-    python-pkgs.markdown
-  ]));
-  eaf-packages = with pkgs; [ nodejs wmctrl xdotool python-dependencies ];
   emacs = pkgs.emacs-gtk;
 in
 {
---
+++ hosts/user/programs/default.nix
@@ -0,0 +1,35 @@
+pkgs:
+
+with pkgs; {
+  web-browsers = [
+  firefox-wayland
+  chromium
+  ];
+  virtualization = [
+    qemu
+  ];
+  containers = [
+    podman
+    distrobox
+  ];
+  productivity = [
+    libreoffice
+    mupdf
+    groff
+  ];
+  development-tools = [
+    plan9port
+    gnumake
+  ];
+  multimedia = [
+    mpv
+    yt-dlp
+  ];
+  misc = [
+    neofetch
+  ];
+  console-games = [
+    crawl
+    nethack
+  ];
+}

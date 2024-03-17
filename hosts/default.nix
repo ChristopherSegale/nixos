f255490afefc7f502c41555164df75f44897574e
ChristopherSegale <ccs.segale@gmail.com>
2024-03-17 19:58:04 -0400

    Refactoring hosts management

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,14 +3,17 @@
 let
   system = "x86_64-linux";
   pkgs = import nixpkgs { inherit system; };
-  lib = nixpkgs.lib;
+  inherit (nixpkgs.lib) nixosSystem;
 in
-builtins.mapAttrs
-  (name: value: lib.nixosSystem {
-    inherit system;
-    modules = import ./modules.nix { host = "${name}"; inherit home-manager snake; }; }
-  ) {
-      G752 = null;
-      QubesOS = null;
+#builtins.mapAttrs
+#  (name: value: lib.nixosSystem {
+#    inherit system;
+#    modules = import ./modules.nix { host = "${name}"; inherit home-manager snake; }; }
+#  ) {
+#      G752 = null;
+#      QubesOS = null;
+#}
+import ./modules.nix {
+  inherit nixosSystem system home-manager snake;
 }
 
--- hosts/modules.nix
+++ hosts/modules.nix
@@ -1,39 +1,45 @@
-{ host, home-manager, snake }:
+{ nixosSystem, system, home-manager, snake }:
 
 { 
-  "G752" = [
-    ./hardware/G752
-    ./hardware/boot/efi.nix
-    ../defaults
-    ./user/baremetal
-    home-manager.nixosModules.home-manager {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = { user = "csegale"; inherit snake; };
-      home-manager.users.csegale = {
-        imports = [(import ./user/baremetal/home.nix)];
-      };
-    }
-    ../graphics/nvidia.nix
-    ../desktop/plasma.nix
-    ../desktop/pipewire.nix
-    ../desktop/printing.nix
-  ];
-  "QubesOS" = [
-    ./hardware/QubesOS
-    ./hardware/boot/mbr.nix
-    ../defaults
-    ./user/QubesOS
-    home-manager.nixosModules.home-manager {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = { user = "user"; };
-      home-manager.users.user = {
-        imports = [(import ./user/QubesOS/home.nix)];
-      };
-    }
-    ../desktop/plasma.nix
-    ../desktop/pipewire.nix
-    ../desktop/printing.nix
-  ];
-}."${host}"
+  "G752" = nixosSystem {
+    inherit system;
+    modules = [
+      ./hardware/G752
+      ./hardware/boot/efi.nix
+      ../defaults
+      ./user/baremetal
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { user = "csegale"; inherit snake; };
+        home-manager.users.csegale = {
+          imports = [(import ./user/baremetal/home.nix)];
+        };
+      }
+      ../graphics/nvidia.nix
+      ../desktop/plasma.nix
+      ../desktop/pipewire.nix
+      ../desktop/printing.nix
+    ];
+  };
+  "QubesOS" = nixosSystem {
+    inherit system;
+    modules = [
+      ./hardware/QubesOS
+      ./hardware/boot/mbr.nix
+      ../defaults
+      ./user/QubesOS
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { user = "user"; };
+        home-manager.users.user = {
+          imports = [(import ./user/QubesOS/home.nix)];
+        };
+      }
+      ../desktop/plasma.nix
+      ../desktop/pipewire.nix
+      ../desktop/printing.nix
+    ];
+  };
+}

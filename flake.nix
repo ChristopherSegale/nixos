106cbd6a9e8c6a55dca4d77e505c18bf6a55e5eb
ChristopherSegale <ccs.segale@gmail.com>
2025-01-24 00:36:36 -0500

    Updating to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Flake for managing the configuration of various hosts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     drawterm.url = "github:ChristopherSegale/drawterm-flake";

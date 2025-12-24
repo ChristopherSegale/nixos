cec5b1456f5005368171a2561c5515ab7ef78e74
ChristopherSegale <ccs.segale@gmail.com>
2025-12-23 21:22:32 -0500

    Removing amdvlk

--- modules/graphics/amd.nix
+++ modules/graphics/amd.nix
@@ -10,11 +10,4 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware.graphics.extraPackages = with pkgs; [
-    amdvlk
-  ];
-  # For 32 bit applications 
-  hardware.graphics.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
-  ];
 }
